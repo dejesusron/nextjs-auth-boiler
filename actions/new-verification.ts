7c6eacb99d51e45ae992f214a2a5aabb0e8781b2
lhc-dejesusron <r.dejesus@lionheart.asia>
2025-07-25 11:46:12 +0800

    working on verification email

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -1,5 +1,41 @@
-// "use server"
+"use server";
 
-// import { prisma } from "@/lib/prisma"
-// import { getUserByEmail } from "@/data/user"
-// import { getVerificationTokenByToken } from "@/data/verification-token"
+import { prisma } from "@/lib/prisma";
+import { getUserByEmail } from "@/data/user";
+import { getVerificationTokenByToken } from "@/data/verification-token";
+
+export async function newVerification(token: string) {
+  const existingToken = await getVerificationTokenByToken(token);
+
+  if (!existingToken) {
+    return { error: "Token does not exist!" };
+  }
+
+  const hasExpired = new Date(existingToken.expires) < new Date();
+
+  if (hasExpired) {
+    return { error: "Token has expired!" };
+  }
+
+  const existingUser = await getUserByEmail(existingToken.email);
+
+  if (!existingUser) {
+    return { error: "Email does not exist!" };
+  }
+
+  await prisma.user.update({
+    where: {
+      id: existingUser.id,
+    },
+    data: {
+      emailVerified: new Date(),
+      email: existingToken.email,
+    },
+  });
+
+  await prisma.verificationToken.delete({
+    where: { id: existingToken.id },
+  });
+
+  return { success: "Email verified!" };
+}
--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -3,15 +3,33 @@
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export function NewVerificationForm() {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
   const searchParams = useSearchParams();
 
   const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -24,7 +42,9 @@ export function NewVerificationForm() {
       backButtonLabel="Back to login"
       backButtonHref="/auth/login">
       <div className="flex items-center w-full justify-center">
-        <BeatLoader />
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
