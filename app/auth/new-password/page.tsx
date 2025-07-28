f8133660f92093a8b176aa8f01ad2eec75318e32
lhc-dejesusron <r.dejesus@lionheart.asia>
2025-07-28 15:18:36 +0800

    password reset and new password done

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NewPasswordForm } from "@/components/auth/new-password-form";
+
+export default function NewPasswordPage() {
+  return (
+    <div>
+      <NewPasswordForm />
+    </div>
+  );
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -18,3 +18,9 @@ export const RegisterSchema = z.object({
 export const ResetSchema = z.object({
   email: z.string().email().min(1, { message: "Email is required" }),
 });
+
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+});
